956cb9ee91438b30df644c4e24658926a5889568
Chris Zias <czias@roborox.com>
2021-07-07 14:49:14 -0400

    some code to start with

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 // basic iffe function
 
-(function(value){
-    
-})()
+(function(dom){
+    var divElement = dom.getElementById('content-search');
+    divElement.classList.add('d-none');
+    divElement.classList.remove('d-none');
+
+    var submitSearchButton = dom.getElementById('submit-search');
+    submitSearchButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('Search Click!');
+    });
+})(document)
 
