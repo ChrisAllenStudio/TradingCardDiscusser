982e20e6d37e403de4ec68584dd75a1c5076c9bd
Chris Zias <czias@roborox.com>
2021-07-09 12:06:27 -0400

    fix #10 light cleanup

--- js/app.js
+++ js/app.js
@@ -1,53 +1,32 @@
+
 // basic iffe function
-/*
-(function(dom){
-    var divElement = dom.getElementById('content-search');
-    divElement.classList.add('d-none');
-    divElement.classList.remove('d-none');
 
-    var submitSearchButton = dom.getElementById('submit-search');
-    submitSearchButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log('Search Click!');
-    });
-})()
+(function(){
+    // get elemnent id is faster than queryselect
+    const submitSearch = document.getElementById('submit-search'); 
+    // header-search
+    // search-container
 
-*/
+    function toggleSearchTextboxVisiblity() {
+    
+        const headerSearch = document.getElementById('header-search'); 
+        const searchContainer = document.getElementById('search-container');
 
+        if (headerSearch.classList.contains('d-none')){ // if headersearch contains d-none as a class
+            headerSearch.classList.remove('d-none'); // then remove the class
+        } else { //if not
+            headerSearch.classList.add('d-none'); // then add d-none as the class
+        }
 
-
-
-
-// get elemnent id is faster than queryselect
-const toggleBtn = document.getElementById('submit-search'); 
-const divList = document.getElementById('search-container');
-const topSearch = document.getElementById('search-bar-top');
-const topBtn = document.getElementById('top-button-search'); 
-
-// header-search
-// search-container
-
-function toggleSearchTextboxVisiblity() {
-    const headerSearch = document.getElementById('header-search'); 
-    const searchContainer = document.getElementById('search-container');
-
-    if (headerSearch.classList.contains('d-none')){ // if headersearch contains d-none as a class
-        headerSearch.classList.remove('d-none'); // then remove the class
-    } else { //if not
-        headerSearch.classList.add('d-none'); // then add d-none as the class
+        if (searchContainer.classList.contains('d-none')){
+            searchContainer.classList.remove('d-none');
+        } else {
+            searchContainer.classList.add('d-none');
+        }
     }
 
-    if (searchContainer.classList.contains('d-none')){
-        searchContainer.classList.remove('d-none');
-    } else {
-        searchContainer.classList.add('d-none');
-    }
-}
-
-toggleBtn.addEventListener('click', () => { // when button is clicked
-    toggleSearchTextboxVisiblity(); // activate the function togglesearchtextbox visibility
-})
-
+    submitSearch.addEventListener('click', toggleSearchTextboxVisiblity);
+})();
 
 
 /***
