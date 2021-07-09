f633de5fd64cac8cec79e3d42ffb2d59b0c44f42
Josh Engle <jengle@roborox.com>
2021-07-09 00:03:31 -0400

    Fix #6 hide and show search boxes now works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // basic iffe function
-
+/*
 (function(dom){
     var divElement = dom.getElementById('content-search');
     divElement.classList.add('d-none');
@@ -10,5 +10,50 @@
         e.preventDefault();
         console.log('Search Click!');
     });
-})(document)
+})()
 
+*/
+
+
+
+
+
+// get elemnent id is faster than queryselect
+const toggleBtn = document.getElementById('submit-search'); 
+const divList = document.getElementById('search-container');
+const topSearch = document.getElementById('search-bar-top');
+const topBtn = document.getElementById('top-button-search'); 
+
+// header-search
+// search-container
+
+function toggleSearchTextboxVisiblity() {
+    const headerSearch = document.getElementById('header-search'); 
+    const searchContainer = document.getElementById('search-container');
+
+    if (headerSearch.classList.contains('d-none')){ // if headersearch contains d-none as a class
+        headerSearch.classList.remove('d-none'); // then remove the class
+    } else { //if not
+        headerSearch.classList.add('d-none'); // then add d-none as the class
+    }
+
+    if (searchContainer.classList.contains('d-none')){
+        searchContainer.classList.remove('d-none');
+    } else {
+        searchContainer.classList.add('d-none');
+    }
+}
+
+toggleBtn.addEventListener('click', () => { // when button is clicked
+    toggleSearchTextboxVisiblity(); // activate the function togglesearchtextbox visibility
+})
+
+
+
+/***
+
+let anchorElement = document.createElement('a');
+anchorElement.id = 'card-name<djld>';
+
+div.append(anchorElement);
+*/
