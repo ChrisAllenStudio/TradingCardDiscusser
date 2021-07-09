c2cecbd2ed45eea4284aa1c489d0c7e9e5b54d47
Chris Zias <czias@chrisallenstudio.net>
2021-07-09 09:39:05 -0400

    #12 add gulpfile that will have the compileSass command

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://gulpjs.com/
+Note to Devs:  Run from the commandline -> npm install --global gulp-cli@2.3.0
+*/
